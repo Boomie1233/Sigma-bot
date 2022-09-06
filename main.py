d176f5a5af70e0af3af58e051a5d48c795191f96
poda thendi <jonathanjohnphilip@gmail.com>
2022-09-06 07:29:13 +0530

    all features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 import os
+from dotenv import load_dotenv
 import nextcord
 import random
 from PIL import Image
@@ -8,9 +8,10 @@ from nextcord import Interaction
 from nextcord import Intents
 from nextcord.ext import commands
 
+load_dotenv()
 intents = Intents.default()
 intents.members = True
-intents.message_content = True
+
 
 
 bot = commands.Bot(command_prefix='-', intents=intents, case_insensitive=True)
@@ -49,7 +50,7 @@ async def ping(ctx):
     await ctx.send(f"{round(bot.latency * 1000)} ms")
 
 
-@bot.slash_command(name = "rickroll", description="Rickrolls you", guild_ids=[808704986485620736, 985909972971950110])
+@bot.slash_command(name = "rickroll", description="Rickrolls you", guild_ids=[808704986485620736, 985909972971950110, 1010526444646060062])
 async def rickroll(interaction: Interaction):
     await interaction.response.send_message("https://tenor.com/view/rick-astly-rick-rolled-gif-22755440"
                    )
@@ -58,7 +59,7 @@ async def rickroll(interaction: Interaction):
 
 
 
-@bot.slash_command(name = "dhillan", description= "gives dhillan dialogues", guild_ids= [ 808704986485620736,985909972971950110])
+@bot.slash_command(name = "dhillan", description= "gives dhillan dialogues", guild_ids= [ 808704986485620736,985909972971950110,1010526444646060062])
 async def dhillan(interaction: Interaction):
     dhillan = [
         "all hates me anyways",
@@ -85,7 +86,7 @@ async def dhillan(interaction: Interaction):
     await interaction.response.send_message(dhillan)
 
 
-@bot.slash_command(name = "hecker", description= "Gives hecker dialogues" , guild_ids= [ 808704986485620736,985909972971950110])
+@bot.slash_command(name = "hecker", description= "Gives hecker dialogues" , guild_ids= [ 808704986485620736,985909972971950110, 1010526444646060062])
 async def hecker(interaction: Interaction):
     damn = [
         "kulukulukuluku wassup guys", "djal fdlskjafkldjas;fd",
@@ -100,7 +101,7 @@ async def hecker(interaction: Interaction):
     await interaction.response.send_message(damn)
 
 
-@bot.slash_command(name = "emil", description= "Gives emil dialogues", guild_ids= [808704986485620736,985909972971950110] )
+@bot.slash_command(name = "emil", description= "Gives emil dialogues", guild_ids= [808704986485620736,985909972971950110, 1010526444646060062] )
 async def emil(interaction:Interaction):
       emil = [
         "ninde achan pavam for having an idiot like you", "jk.....ROWLING",
@@ -112,13 +113,13 @@ async def emil(interaction:Interaction):
 
 
 
-@bot.slash_command(name = "toss", description = "give heads or tails",  guild_ids= [808704986485620736,985909972971950110])
+@bot.slash_command(name = "toss", description = "give heads or tails",  guild_ids= [808704986485620736,985909972971950110, 1010526444646060062])
 async def toss(interaction:Interaction):
      coin = ["heads", "tails"]
      coin = random.choice(coin)
      await interaction.response.send_message(coin)
 
-@bot.slash_command(name = "thendis", description = "give thendis",  guild_ids= [808704986485620736,985909972971950110])
+@bot.slash_command(name = "thendis", description = "give thendis",  guild_ids= [808704986485620736,985909972971950110, 1010526444646060062])
 async def thendis(interaction:Interaction):
      thendis = [ "./pics/amna_edit.jpg", "./pics/nice farriz.jpg", "./pics/saket.jpg", "./pics/amna_edit.jpg"]
      thendis= random.choice(thendis)
@@ -169,4 +170,4 @@ async def patti(ctx, user:nextcord.Member):
 
 
 
-bot.run("OTI3Nzk5NTA0Mjg2MTIyMDI0.YdPenw.izbYQtDJAmZ6lgfn77t9aMXtyVo")
+bot.run(os.getenv("TOKEN"))
