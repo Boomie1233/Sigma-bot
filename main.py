85cde93e803ee0a82fd443579a24ab7b328597ba
Boomie1233 <jonathanjohnphilip28@gmail.com>
2026-02-06 21:56:44 +0530

    Fixed hosting issues

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 import nextcord
+from aiohttp import web
 from nextcord import Intents
 from nextcord.ext import commands
+import asyncio
 
 load_dotenv()
 intents = Intents.default()
@@ -22,23 +24,35 @@ async def on_ready():
     await bot.change_presence(activity=nextcord.Game('-help'))
     print('bot is ready')
   
+async def handle_health(request):
+    return web.Response(text="OK", status=200)
 
-     
+async def start():
+    app = web.Application()
+    app.router.add_get('/', handle_health)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    port = int(os.environ.get("PORT", 8000))
+    site = web.TCPSite(runner, '0.0.0.0', port)
+    await site.start()
+    print("Web server opened")
   
-
-for cog in os.listdir(directory):
-    if cog.endswith(".py"):
-        try:
-            cog = f"cogs.{cog.replace('.py', '')}"
-            bot.load_extension(cog)
-        except Exception as e:
-            print(f"{cog} Can not be loaded")
-            raise e
-
+async def main():
+    for cog in os.listdir(directory):
+        if cog.endswith(".py"):
+            try:
+                bot.load_extension(cog)
+                cog = f"cogs.{cog.replace('.py', '')}"
+            except Exception as e:
+                print(f"{cog} Can not be loaded")
+                raise e
+    await start()
+    await bot.start(os.getenv("TOKEN"))
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{round(bot.latency * 1000)} ms")
 
-bot.run(os.getenv("TOKEN"))
+if __name__ == "__main__":
+    asyncio.run(main())
