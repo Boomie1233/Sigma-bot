4ed86ef71a0fb341ce399c200eb99686367a81ae
Boomie1233 <jonathanjohnphilip28@gmail.com>
2026-02-06 22:48:30 +0530

    Fixed hosting issues part2

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import os
 from dotenv import load_dotenv
 import nextcord
-from aiohttp import web
+from flask import Flask
 from nextcord import Intents
+from threading import Thread
 from nextcord.ext import commands
-import asyncio
 
 load_dotenv()
 intents = Intents.default()
 intents.members = True
 intents.messages = True
 intents.message_content = True
+
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return 'Bot is running!'
+
+def run_flask():
+    port = int(os.environ.get('PORT', 8000))
+    print("Web server started")
+    app.run(host='0.0.0.0', port=port)
 
 directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cogs")
 
@@ -19,35 +30,23 @@ bot = commands.Bot(command_prefix='-', intents=intents, case_insensitive=True)
 bot.remove_command('help')
 
 
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=nextcord.Game('-help'))
     print('bot is ready')
   
-async def handle_health(request):
-    return web.Response(text="OK", status=200)
+     
 
-async def start():
-    app = web.Application()
-    app.router.add_get('/', handle_health)
-    runner = web.AppRunner(app)
-    await runner.setup()
-    port = int(os.environ.get("PORT", 8000))
-    site = web.TCPSite(runner, '0.0.0.0', port)
-    await site.start()
-    print("Web server opened")
-  
-async def main():
-    for cog in os.listdir(directory):
-        if cog.endswith(".py"):
-            try:
-                cog = f"cogs.{cog.replace('.py', '')}"
-                await bot.load_extension(cog)
-            except Exception as e:
-                print(f"{cog} Can not be loaded")
-                raise e
-    await start()
-    await bot.start(os.getenv("TOKEN"))
+for cog in os.listdir(directory):
+    if cog.endswith(".py"):
+        try:
+            cog = f"cogs.{cog.replace('.py', '')}"
+            bot.load_extension(cog)
+        except Exception as e:
+            print(f"{cog} Can not be loaded")
+            raise e
+
 
 
 @bot.command()
@@ -55,4 +54,6 @@ async def ping(ctx):
     await ctx.send(f"{round(bot.latency * 1000)} ms")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    server_thread = Thread(target=run_flask)
+    server_thread.start()
+    bot.run(os.getenv('TOKEN'))
